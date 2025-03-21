6c5931dcf71dc5bf926cbddef9602ace4923939b
Morezagholami <morezagho@gmail.com>
2025-03-22 00:32:56 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+/*
+  Just Start the program from here and do nothing else here.
+ */
+
+import views.AppView;
+
+public class Main {
+    public static void main(String[] args) {
+      new AppView().run();
+}
+}
+
